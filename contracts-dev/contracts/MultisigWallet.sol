1274d1d0cdfcf0bfce76381eea779316362df338
Nonit Mittal <nonitmittal@gmail.com>
2022-08-20 13:29:29 +0530

    add initial contract dev setup

---
+++ contracts-dev/contracts/MultisigWallet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title MultisigWallet
+ * @dev Implements multisig wallet along with rights delegation
+ */
+contract MultisigWallet{
+    
+}
