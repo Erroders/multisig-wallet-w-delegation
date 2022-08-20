abfaf22786c26ad631368e4312b17e669bc15dee
Nonit Mittal <nonitmittal@gmail.com>
2022-08-20 20:08:46 +0530

    add deploy script

--- contracts-dev/hardhat.config.ts
+++ contracts-dev/hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+
+require('dotenv').config({ path: __dirname + '/.env' });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+        url: process.env.MUMBAI_ALCHEMY_URL || '',
+        accounts: [`0x${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
+    },
+},
 };
 
 export default config;
