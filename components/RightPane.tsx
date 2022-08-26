876d0318d32fafc0ca94896c24f7f7af5a5bb9eb
Sparsh Agarwal <sparshaga25@gmail.com>
2022-08-26 19:39:58 +0530

    Created Add Signer Panel

--- components/RightPane.tsx
+++ components/RightPane.tsx
@@ -3,14 +3,14 @@ import makeBlockie from "ethereum-blockies-base64";
 import { ethers } from "ethers";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
-import Router from "next/router";
 import { useState } from "react";
 import { useSignerContext } from "../contexts/Signer";
+import { addSigner } from "../controllers/SignerController";
 import {
   createTransaction,
   deposit,
 } from "../controllers/TransactionController";
-import { Wallet } from "../utils/types";
+import { Signer, SignerMetadata, Wallet } from "../utils/types";
 import TransactionCard from "./TransactionCard";
 TimeAgo.setDefaultLocale(en.locale);
 TimeAgo.addLocale(en);
@@ -19,12 +19,13 @@ type Props = { wallet: Wallet };
 
 const RightPane = ({ wallet }: Props) => {
   const [typeOfToken, setTypeOfToken] = useState(0);
+  const [showAddSignerBox, setShowAddSignerBox] = useState(true);
 
   const timeAgo = new TimeAgo("en-US");
   const { signer } = useSignerContext();
   return (
-    <div className="col-span-4">
-      <section className="sticky top-0 grid h-72 grid-cols-5 bg-purple-800 px-10 text-white">
+    <div className="col-span-4 bg-gray-100">
+      <section className="sticky top-0 z-20 grid h-72 grid-cols-5 bg-purple-800 px-10 text-white">
         <div className="col-span-3 my-auto">
           <div className="flex items-center justify-center space-x-10">
             <div
@@ -80,11 +81,11 @@ const RightPane = ({ wallet }: Props) => {
                     Deposit
                   </button>
                 )}
-                {signer && (
+                {signer && !showAddSignerBox && (
                   <button
                     className="btn-blue"
                     onClick={() => {
-                      Router.push("/addSigner");
+                      setShowAddSignerBox(true);
                     }}
                   >
                     Add Signer
@@ -161,7 +162,155 @@ const RightPane = ({ wallet }: Props) => {
           </form>
         </div>
       </section>
-      <section className="flex flex-col space-y-3 px-10 py-20">
+
+      <section
+        className={
+          "flex flex-col space-y-3 bg-gray-100 px-10 py-10" +
+          (showAddSignerBox ? "" : " hidden ")
+        }
+      >
+        <div className="flex justify-between">
+          <h2 className="my-auto  text-xl font-semibold">Add Signer</h2>
+
+          <button
+            className="btn-dark"
+            onClick={() => {
+              setShowAddSignerBox(false);
+            }}
+          >
+            <span className="mr-2">x</span> Close
+          </button>
+        </div>
+
+        <form
+          action=""
+          className="space-y-4"
+          onSubmit={(e: any) => {
+            e.preventDefault();
+            const data: SignerMetadata = {
+              name: e.target.name.value,
+              contactNo: e.target.number.value,
+              email: e.target.email.value,
+              walletAddress: e.target.walletAddress.value,
+              role: e.target.role.value,
+              remarks: e.target.remarks.value,
+            };
+            const signerData: Signer = {
+              address: e.target.walletAddress.value,
+              weight: 0,
+              delegateTo: "",
+              metadata: data as SignerMetadata,
+              signer: null,
+            };
+
+            addSigner(signer!.signer, signerData);
+          }}
+        >
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="name" className="text-sm font-medium">
+                Name
+              </label>
+
+              <div className="relative mt-1">
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-md"
+                  placeholder="Enter Name"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="number" className="text-sm font-medium">
+                Number
+              </label>
+
+              <div className="relative mt-1">
+                <input
+                  type="tel"
+                  id="number"
+                  className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-md"
+                  placeholder="Enter Number"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="address" className="text-sm font-medium">
+              Wallet Address
+            </label>
+
+            <div className="relative mt-1">
+              <input
+                type="text"
+                id="walletAddress"
+                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-md"
+                placeholder="Enter Wallet Address"
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="email" className="text-sm font-medium">
+                Email
+              </label>
+
+              <div className="relative mt-1">
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-md"
+                  placeholder="Enter email"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="Role" className="text-sm font-medium">
+                Role
+              </label>
+
+              <div className="relative mt-1">
+                <input
+                  type="text"
+                  id="role"
+                  className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-md"
+                  placeholder="Enter Role"
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <label className="text-sm font-medium " htmlFor="remarks">
+              Message
+            </label>
+            <textarea
+              className="w-full rounded-lg border-gray-200 p-3 text-sm shadow-md"
+              placeholder="Remarks"
+              rows={8}
+              id="remarks"
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+          >
+            Add Signer
+          </button>
+        </form>
+      </section>
+
+      <section
+        className={
+          "flex flex-col space-y-3 px-10 py-20" +
+          (showAddSignerBox ? " hidden " : "")
+        }
+      >
         {wallet.transactions.map((transaction) => {
           return <TransactionCard key={transaction.id} txn={transaction} />;
         })}
