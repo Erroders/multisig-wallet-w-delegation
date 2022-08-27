1bd59c31d837b24b26ab5fa77f2eb66ebfd52de9
Sparsh Agarwal <sparshaga25@gmail.com>
2022-08-27 15:36:14 +0530

    Updated Index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import LeftPane from "../components/LeftPane";
 import RightPane from "../components/RightPane";
-import { executeQuery } from "../utils/apolloClient";
-import { Signer, Transaction, Wallet } from "../utils/types";
 import { useSignerContext } from "../contexts/Signer";
+import { executeQuery } from "../utils/apolloClient";
+import { Signer, Wallet } from "../utils/types";
 
 const Home: NextPage = () => {
-    const [wallet, setWallet] = useState<Wallet | null>(null);
-    const { signer } = useSignerContext();
-    useEffect(() => {
-        const getWalletData = async () => {
-            let data = await executeQuery(`query{
+  const [wallet, setWallet] = useState<Wallet | null>(null);
+  const { signer } = useSignerContext();
+  useEffect(() => {
+    const getWalletData = async () => {
+      let data = await executeQuery(`query{
                 wallets{
                   id
                   lockedBalance
@@ -47,43 +47,49 @@ const Home: NextPage = () => {
                   }
                 }
               }`);
-            data = data.wallets[0];
-            console.log(data);
-            const balance = await signer?.signer?.provider?.getBalance(data.id);
-            const wallet_: Wallet = {
-                contractAddress: data.id,
-                owner: data.owner.address,
-                signers: data.signers ? (data.signers as Signer[]) : [],
-                transactions: data.transactions
-                    ? (data.transactions as Transaction[])
-                    : [],
-                balance: balance?.toString() || "0",
-                lockedBalance: data.lockedBalance,
-            };
-            console.log(wallet_);
-            setWallet(wallet_);
-        };
-        getWalletData();
-    }, [signer]);
+      data = data.wallets[0];
+      console.log(data);
+      const balance = await signer?.signer?.provider?.getBalance(data.id);
+      const wallet_: Wallet = {
+        contractAddress: data.id,
+        owner: data.owner.address,
+        signers: data.signers ? (data.signers as Signer[]) : [],
+        // transactions: data.transactions
+        //   ? (data.transactions as Transaction[])
+        //   : [],
+        // TODO: Remove hardcoded values
+        createdOn: "aaa",
+        erc1155Transactions: [],
+        erc20Transactions: [],
+        erc721Transactions: [],
+        balance: balance?.toString() || "0",
+        lockedBalance: data.lockedBalance,
+      };
+      console.log(wallet_);
+      setWallet(wallet_);
+    };
+    getWalletData();
+  }, [signer]);
 
-    return wallet ? (
-        <div>
-            <Head>
-                <title>Multisig Wallet with Delegation</title>
-                <meta name="description" content="" />
-                {/* <link rel="icon" href="/favicon.ico" /> */}
-            </Head>
+  return wallet ? (
+    <div>
+      <Head>
+        <title>Multisig Wallet with Delegation</title>
+        <meta name="description" content="" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
 
-            <main className="grid grid-cols-6">
-                <LeftPane signers={wallet.signers} />
-                <RightPane wallet={wallet} />
-            </main>
+      <main className="grid grid-cols-6">
+        <LeftPane signers={wallet.signers} />
+        <RightPane wallet={wallet} />
+        {/* <WalletPage /> */}
+      </main>
 
-            <footer></footer>
-        </div>
-    ) : (
-        <></>
-    );
+      <footer></footer>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default Home;
