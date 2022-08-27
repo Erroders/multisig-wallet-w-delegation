467115da7f5417875cd125f0b4c05a08395fe6d5
Vineet Mishra <vineetmishra@jklu.edu.in>
2022-08-27 18:13:34 +0530

    header wallet

---
+++ components/RightPanel/HeaderWalletPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const HeaderWalletPage = (props: Props) => {
+  return (
+    <div>
+        <button className='btnb-blue'>Add a Wallet</button>
+    </div>
+  )
+}
+
+export default HeaderWalletPage
