491510d0a296aba75f6ed22ffb84c34b757ac842
Nonit Mittal <nonitmittal@gmail.com>
2022-11-21 23:10:42 +0530

    - changed the way to upload data to IPFS - used TheGraph API

--- utils/uploadToIPFS.ts
+++ utils/uploadToIPFS.ts
@@ -1,11 +1,20 @@
-import { NFTStorage } from "nft.storage";
+// import { NFTStorage } from "nft.storage";
+import { create } from "ipfs-http-client";
 import { SignerMetadata, WalletMetadata } from "./types";
 
 export async function uploadToIpfs(metadata: SignerMetadata | WalletMetadata) {
-    const client = new NFTStorage({
-        token: process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY || "",
+    // const client = new NFTStorage({
+    //     token: process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY || "",
+    // });
+    // const cid = await client.storeBlob(new Blob([JSON.stringify(metadata)]));
+    // console.log(cid);
+    // return cid;
+
+    // https://www.npmjs.com/package/ipfs-http-client
+    const ipfs = await create({
+        url: "https://api.thegraph.com/ipfs/api/v0/",
     });
-    const cid = await client.storeBlob(new Blob([JSON.stringify(metadata)]));
-    console.log(cid);
-    return cid;
+    const { path } = await ipfs.add(new Blob([JSON.stringify(metadata)]));
+    console.log(path);
+    return path;
 }
